27983b45004d54f8c86cc7313318a99a2299a15a
cdschillaci <schillaci@berkeley.edu>
2014-02-25 22:16:04 -0800

    Add gini and split_gini

--- hw5/gini.py
+++ hw5/gini.py
@@ -5,6 +5,7 @@ Classes are strings."""
 
 import fileinput
 import csv
+import collections as coll
 
 (
     CMTE_ID, AMNDT_IND, RPT_TP, TRANSACTION_PGI, IMAGE_NUM, TRANSACTION_TP,
@@ -21,6 +22,12 @@ CANDIDATES = {
 ############### Set up variables
 # TODO: declare datastructures
 
+
+candidate_count=coll.defaultdict(int)
+zip_count=coll.defaultdict(int)
+
+zip_dicts={}
+
 ############### Read through files
 for row in csv.reader(fileinput.input(), delimiter='|'):
     candidate_id = row[CAND_ID]
@@ -29,14 +36,29 @@ for row in csv.reader(fileinput.input(), delimiter='|'):
 
     candidate_name = CANDIDATES[candidate_id]
     zip_code = row[ZIP_CODE]
+    
+    if zip_code not in zip_dicts:
+        zip_dicts[zip_code]=coll.defaultdict(int)
+        
+    zip_dicts[zip_code][candidate_id]+=1
+    
     ###
     # TODO: save information to calculate Gini Index
     ##/
+    candidate_count[candidate_id]+=1
+    
+print candidate_count
 
-###
-# TODO: calculate the values below:
-gini = 0  # current Gini Index using candidate name as the class
-split_gini = 0  # weighted average of the Gini Indexes using candidate names, split up by zip code
+candidate_counter=coll.Counter()
+candidate_counter.update(candidate_count)
+
+# Total number of records
+total=float(sum(candidate_counter.values()))
+
+gini = 1-sum((float(candidate_counter[cand])/total)**2 for cand in candidate_counter)  # current Gini Index using candidate name as the class
+print "The gini index of the candidates, not split by zip codes, is "+str(gini)
+
+split_gini = sum( (1-sum((zip_dicts[code][cand]/total)**2 for cand in zip_dicts[code]))*sum(zip_dicts[code].values())/total for code in zip_dicts)  # weighted average of the Gini Indexes using candidate names, split up by zip code
 ##/
 
 print "Gini Index: %s" % gini
