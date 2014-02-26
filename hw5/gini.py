e7a259c2d781b03189a1af7889eecded9e7ccd1b
cdschillaci <schillaci@berkeley.edu>
2014-02-25 23:08:21 -0800

    Fixed bugs

--- hw5/gini.py
+++ hw5/gini.py
@@ -7,6 +7,7 @@ import fileinput
 import csv
 import collections as coll
 
+
 (
     CMTE_ID, AMNDT_IND, RPT_TP, TRANSACTION_PGI, IMAGE_NUM, TRANSACTION_TP,
     ENTITY_TP, NAME, CITY, STATE, ZIP_CODE, EMPLOYER, OCCUPATION,
@@ -19,13 +20,10 @@ CANDIDATES = {
     'P80003353': 'Romney',
 }
 
-############### Set up variables
-# TODO: declare datastructures
-
-
+# For calculating the Gini index over the two Candidates
 candidate_count=coll.defaultdict(int)
-zip_count=coll.defaultdict(int)
 
+# We will store a defaultdict(int) of candidates for each zip-code
 zip_dicts={}
 
 ############### Read through files
@@ -35,19 +33,23 @@ for row in csv.reader(fileinput.input(), delimiter='|'):
         continue
 
     candidate_name = CANDIDATES[candidate_id]
-    zip_code = row[ZIP_CODE]
     
+    candidate_count[candidate_id]+=1    
+    
+    # Get only the first 5 digits of zip code
+    zip_code = row[ZIP_CODE][0:5]
+    
+    # Discard if blank
+    if len(zip_code)==0:
+        continue
+        
+    # If the zip_code has not been seen yet, add a new dict
     if zip_code not in zip_dicts:
         zip_dicts[zip_code]=coll.defaultdict(int)
         
+    # Increment the candidate's dictionary entry in the correct zip code
     zip_dicts[zip_code][candidate_id]+=1
     
-    ###
-    # TODO: save information to calculate Gini Index
-    ##/
-    candidate_count[candidate_id]+=1
-    
-print candidate_count
 
 candidate_counter=coll.Counter()
 candidate_counter.update(candidate_count)
@@ -56,10 +58,15 @@ candidate_counter.update(candidate_count)
 total=float(sum(candidate_counter.values()))
 
 gini = 1-sum((float(candidate_counter[cand])/total)**2 for cand in candidate_counter)  # current Gini Index using candidate name as the class
-print "The gini index of the candidates, not split by zip codes, is "+str(gini)
 
-split_gini = sum( (1-sum((zip_dicts[code][cand]/total)**2 for cand in zip_dicts[code]))*sum(zip_dicts[code].values())/total for code in zip_dicts)  # weighted average of the Gini Indexes using candidate names, split up by zip code
-##/
+# Total number of records with a non-blank zip code
+total=float(0)
+for code in zip_dicts:
+    total+=sum(zip_dicts[code].values())
+
+# Use an unreadabe nested for/sum stateent to calculate split gini
+split_gini = sum( (1-sum((zip_dicts[code][cand]/float(sum(zip_dicts[code].values())))**2 for cand in zip_dicts[code]))*sum(zip_dicts[code].values())/total for code in zip_dicts)  # weighted average of the Gini Indexes using candidate names, split up by zip code
+
 
 print "Gini Index: %s" % gini
 print "Gini Index after split: %s" % split_gini
